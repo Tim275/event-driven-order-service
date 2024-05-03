3d7de9a6ff05d0e54eff1ac3725ca71637ecc802
Tim275 <sagichdirnicht259@outlook.de>
2024-05-03 03:37:29 +0200

    change

--- hello-world/tests/unit/lambdas/app.test.mjs
+++ hello-world/tests/unit/lambdas/app.test.mjs
@@ -13,12 +13,8 @@ describe('loadProducts', () => {
     AWSMock.restore('DynamoDB.DocumentClient');
   });
 
-  // Removed the failing test
-
   it('should return 500 when there is an error', async () => {
-    AWSMock.mock('DynamoDB.DocumentClient', 'batchWrite', (params, callback) => {
-      callback(new Error('some error happened'), null);
-    });
+    AWSMock.mock('DynamoDB.DocumentClient', 'batchWrite', Promise.reject(new Error('some error happened')));
 
     const response = await loadProducts();
 
